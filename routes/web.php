a77f7e9bd3142992843ff83099bba27751a1472e
tungcorn <tung01012005s@gmail.com>
2026-02-23 17:13:22 +0700

    feat: register routes for NCC, LoaiHang, MatHang management - Add resource routes for ncc, loai-hang, mat-hang (index, store, update, destroy) - Update demo_orders.sql with NgayNhap data

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
 use App\Http\Controllers\DonNhapHangController;
+use App\Http\Controllers\NCCController;
+use App\Http\Controllers\LoaiHangController;
+use App\Http\Controllers\MatHangController;
 
 // Trang chủ redirect về danh sách đơn nhập hàng
 Route::get('/', function () {
     return redirect()->route('don-nhap.index');
 });
 
-// Resource route: tự động tạo các route CRUD cho đơn nhập hàng
-// (index, create, store, show, edit, update, destroy)
+// NCC routes (index, store, update, destroy)
+Route::resource('ncc', NCCController::class)->only(['index', 'store', 'update', 'destroy']);
+
+// Loại hàng routes
+Route::resource('loai-hang', LoaiHangController::class)->only(['index', 'store', 'update', 'destroy']);
+
+// Mặt hàng routes
+Route::resource('mat-hang', MatHangController::class)->only(['index', 'store', 'update', 'destroy']);
+
+// Đơn nhập hàng (resource đầy đủ)
 Route::resource('don-nhap', DonNhapHangController::class);
